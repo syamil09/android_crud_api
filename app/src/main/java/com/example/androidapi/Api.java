7ac5c4adb9a9fc34b58de5650d9704546a24437d
syamil09 <syamil2502@gmail.com>
2021-01-21 23:27:25 +0700

    :tada: init

---
+++ app/src/main/java/com/example/androidapi/Api.java
@@ -0,0 +1,10 @@
+package com.example.androidapi;
+
+public class Api {
+    private static final String ROOT_URL = "https://apippb.000webhostapp.com/api_ppb/Api.php?apicall=";
+
+    private static final String ADD_URL = ROOT_URL + "add";
+    private static final String READ_URL = ROOT_URL + "get";
+    private static final String UPDATE_URL = ROOT_URL + "update";
+    private static final String DELETE_URL = ROOT_URL + "delete";
+}
---
+++ app/src/main/java/com/example/androidapi/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        spinner = (Spinner) findViewById(R.id.spKomentar);
+//
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.teams, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//        spinner.setAdapter(adapter);
+    }
+}
