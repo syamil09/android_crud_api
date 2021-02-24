166b6c7a5a79955b597d216100c5664a96f6c254
syamil09 <syamil2502@gmail.com>
2021-02-24 18:05:19 +0700

    :sparkles: update

--- app/src/main/java/com/example/androidapi/Api.java
+++ app/src/main/java/com/example/androidapi/Api.java
@@ -1,10 +1,10 @@
 package com.example.androidapi;
 
 public class Api {
-    private static final String ROOT_URL = "https://apippb.000webhostapp.com/api_ppb/Api.php?apicall=";
+    public static final String ROOT_URL = "https://apippb.000webhostapp.com/api_ppb/Api.php?apicall=";
 
-    private static final String ADD_URL = ROOT_URL + "add";
-    private static final String READ_URL = ROOT_URL + "get";
-    private static final String UPDATE_URL = ROOT_URL + "update";
-    private static final String DELETE_URL = ROOT_URL + "delete";
+    public static final String ADD_URL = ROOT_URL + "add";
+    public static final String READ_URL = ROOT_URL + "get";
+    public static final String UPDATE_URL = ROOT_URL + "update";
+    public static final String DELETE_URL = ROOT_URL + "delete";
 }
